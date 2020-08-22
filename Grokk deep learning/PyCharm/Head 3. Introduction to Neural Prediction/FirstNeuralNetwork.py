a150c3e647f653bb6adbb803461a4d15cd147aaf
Po4aPo <suhar20001@gmail.com>
2020-08-22 17:14:15 +0400

    Grokking DL v.2

--- Grokk deep learning/PyCharm/Head 3. Introduction to Neural Prediction/FirstNeuralNetwork.py
+++ Grokk deep learning/PyCharm/Head 3. Introduction to Neural Prediction/FirstNeuralNetwork.py
@@ -1,10 +1,18 @@
-weight = 0.1
-
-def neural_network(input, weight):
+def neural_network_num_one(input, weight):
+    '''
+    This neural network makes a prediction of the outcome
+     of the match from number of games played
+    -------------------------------------------------
+    Эта нейронная сеть делает прогноз матча исхода из
+    количества сыгранных игр
+    '''
     predict = input * weight
     return  predict
 
-number_of_toes = [8.5,9.5,10,9]
-input = number_of_toes[0]
-pred = neural_network(input, weight)
-print(pred)
+def program_launch():
+    weight = 0.1
+    number_of_toes = [8.5,9.5,10,9]
+    input = number_of_toes[0]
+    pred = neural_network_num_one(input, weight)
+    print(pred)
+
